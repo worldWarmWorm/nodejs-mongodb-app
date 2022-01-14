0523a80fe9ae277b0a1fe4d4fe6d4c03b420ffa4
worldwarmworm <st.valeriq@gmail.com>
2022-01-14 13:50:32 +0700

    add views for course cart and  edit course form, realised updating of the course data via form

--- routes/courses.js
+++ routes/courses.js
@@ -12,4 +12,31 @@ router.get('/', async (req, res) => {
   })
 })
 
+router.get('/:id', async (req, res) => {
+  const course = await Course.getById(req.params.id)
+  res.render('course', {
+    layout: 'empty',
+    title: `Курс ${course.title}`,
+    course
+  })
+})
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
+
+  const course = await Course.getById(req.params.id)
+
+  res.render('course-edit', {
+    title: `Редактировать курс ${course.title}`,
+    course
+  })
+})
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body)
+  res.redirect('/courses')
+})
+
 module.exports = router
